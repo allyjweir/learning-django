1ce1da6d671b22a1110a7f7a0c7b213d39c16bfb
allyjweir <ally.pcgf+git@gmail.com>
2013-10-17 10:49:19 +0100

    Got up to ch4.1 in textbook

--- tango_with_django_project/tango_with_django_project/settings.py
+++ tango_with_django_project/tango_with_django_project/settings.py
@@ -111,6 +111,8 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    '/Users/alastairweir/djangotests/tango_with_django_project/templates', 
+
 )
 
 INSTALLED_APPS = (
@@ -124,6 +126,7 @@ INSTALLED_APPS = (
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
+    'rango',
 )
 
 SESSION_SERIALIZER = 'django.contrib.sessions.serializers.JSONSerializer'
