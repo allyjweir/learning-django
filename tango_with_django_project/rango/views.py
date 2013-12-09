cd125910a07dd8b31e0531a579e8034c65ca68fa
allyjweir <ally.pcgf@gmail.com>
2013-12-09 15:55:46 +0000

    missed P in regEx in urls.py

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -8,6 +8,10 @@ def index(request):
     context = RequestContext(request)
     category_list = Category.objects.order_by('-likes')[:5]
     context_dict = {'categories' : category_list}
+
+    for category in category_list:
+        category.url = category.name.replace(' ','_')
+
     return render_to_response('rango/index.html', context_dict, context)
 
 def about(request):
