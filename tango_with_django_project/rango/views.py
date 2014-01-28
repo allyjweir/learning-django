4baf7db45e71b2dae25be03ace5f7f48290dfec5
allyjweir <ally.pcgf@gmail.com>
2014-01-28 14:54:59 +0000

    Chapter 7 completed

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -3,6 +3,7 @@ from django.template import RequestContext
 from django.shortcuts import render_to_response
 from rango.models import Category
 from rango.models import Page
+from rango.forms import CategoryForm
 
 def index(request):
     context = RequestContext(request)
@@ -31,3 +32,31 @@ def category(request, category_name_url):
         pass
 
     return render_to_response('rango/category.html', context_dict, context)
+    
+def add_category(request):
+    # Get the context from the request.
+    context = RequestContext(request)
+
+    # A HTTP POST?
+    if request.method == 'POST':
+        form = CategoryForm(request.POST)
+
+        # Have we been provided with a valid form?
+        if form.is_valid():
+            # Save the new category to the database.
+            form.save(commit=True)
+
+            # Now call the index() view.
+            # The user will be shown the homepage.
+            return index(request)
+        else:
+            # The supplied form contained errors - just print them to the terminal.
+            print form.errors
+    else:
+        # If the request was not a POST, display the form to enter details.
+        form = CategoryForm()
+
+    # Bad form (or form details), no form supplied...
+    # Render the form with error messages (if any).
+    return render_to_response('rango/add_category.html', {'form': form}, context)
+
